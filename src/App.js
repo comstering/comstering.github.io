021c867be8ed804b239421440536cad09a4d4c25
comstering <coms1101@naver.com>
2021-05-03 19:04:18 +0900

    Made Nav rendering

--- src/App.js
+++ src/App.js
@@ -1,33 +1,14 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import Footer from './components/views/Footer/Footer';
 
 import NavBar from './components/views/NavBar/NavBar';
-import MainPage from './components/views/MainPage/MainPage';
-import AboutPage from './components/views/AboutPage/AboutPage';
-import ProjectPage from './components/views/ProjectPage/ProjectPage';
-import SkillPage from './components/views/SkillPage/SkillPage';
-import ContactPage from './components/views/ContactPage/ContactPage';
 
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path='/' component={MainPage} />
-
-        <Route path='/about' component={AboutPage} />
-
-        <Route path="/project" component={ProjectPage} />
-
-        <Route path='/skills' component={SkillPage} />
-
-        <Route path='/contact' component={ContactPage} />
-      </Switch>
-    </Router>
+    <div>
+    <NavBar />
+    <Footer />
+    </div>
   );
 }
 
--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,21 +1,55 @@
-import React from 'react'
-import { Navbar, Nav} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import { Navbar, Nav } from 'react-bootstrap';
+
+import MainPage from '../MainPage/MainPage';
+import AboutPage from '../AboutPage/AboutPage';
+import ProjectPage from '../ProjectPage/ProjectPage';
+import SkillPage from '../SkillPage/SkillPage';
+import ContactPage from '../ContactPage/ContactPage';
+import '../../css/fonts.css'
+
+function returnPage(page) {
+  if(page === 'home') {
+    return MainPage;
+  }
+  else if (page ==='about') {
+    return AboutPage;
+  }
+  else if(page === 'project') {
+    return ProjectPage;
+  }
+  else if(page === 'skills') {
+    return SkillPage;
+  } else if(page === 'contact') {
+    return ContactPage;
+  }
+}
 
 function NavBar() {
+  const [page, setPage] = useState('home')
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Comstering</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href='/'>Home</Nav.Link>
-          <Nav.Link href='/about'>About Me</Nav.Link>
-          <Nav.Link href='/project'>Project</Nav.Link>
-          <Nav.Link href='/skills'>Skills</Nav.Link>
-          <Nav.Link href='/contact'>Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <Router>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand className='cursor_test' onClick={() => setPage('home')}>Comstering</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link onClick={() => setPage('home')}>Home</Nav.Link>
+            <Nav.Link onClick={() => setPage('about')}>About Me</Nav.Link>
+            <Nav.Link onClick={() => setPage('project')}>Project</Nav.Link>
+            <Nav.Link onClick={() => setPage('skills')}>Skills</Nav.Link>
+            <Nav.Link onClick={() => setPage('contact')}>Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+
+      <Route component={returnPage(page)} />
+    </Router>
   )
 }
 
