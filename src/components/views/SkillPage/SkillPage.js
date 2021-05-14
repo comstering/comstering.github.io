16d16f226b2e79885dcf6758f42af29e5bbfc9ac
comstering <coms1101@naver.com>
2021-05-15 01:06:16 +0900

    SkillPage set up comfiguration

--- src/components/views/SkillPage/SkillPage.js
+++ src/components/views/SkillPage/SkillPage.js
@@ -3,7 +3,32 @@ import React from 'react'
 function SkillPage() {
     return (
         <div>
-            SkillPage
+            <h2>Develope</h2>
+            <div>
+                <h4>Language</h4>
+                <div>
+                    <p>python</p>
+                    <p>javascript</p>
+                </div>
+                <h4>DataBase</h4>
+                <div>
+                    <p>MySQL</p>
+                </div>
+                <h4>Deploy</h4>
+                <div>
+                    <p>Github</p>
+                </div>
+                <h4>Sub Language</h4>
+                <div>
+                    <p>Java</p>
+                    <p>C</p>
+                </div>
+            </div>
+            <h2>Flatform</h2>
+            <div>
+                <p>windows</p>
+                <p>Linux</p>
+            </div>
         </div>
     )
 }
