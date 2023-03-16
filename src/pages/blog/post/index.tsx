ae9ed39306dec6269eefb916b5f5535abd766367
comstering <coms1101@gmail.com>
2023-03-17 00:17:51 +0900

    ✨ feat: Implement index page of post

--- src/pages/blog/post/index.tsx
+++ src/pages/blog/post/index.tsx
@@ -1,7 +1,36 @@
 import Head from "next/head";
+import { getSortedPostsData } from "@/lib/posts";
 
-export default function Post() {
-    <Head>
-        <title>Comstering Blog</title>
-    </Head>
+export const getStaticProps = () => {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData
+        }
+    }
 }
+
+const Post = ({ allPostsData }: {
+    allPostsData: {
+    id: string,
+    date: string,
+    title: string
+}[]
+}) => {
+    <>
+        <Head>
+            <title>Comstering Blog</title>
+        </Head>
+        <ul>
+            { allPostsData.map(({ id, date, title }: {id: string, date: string, title: string}) => (
+                <li key={id}>
+                    <a>{title}</a>
+                    <br />
+                    <small>{date}</small>
+                </li>
+            ))}
+        </ul>
+    </>
+}
+
+export default Post;
