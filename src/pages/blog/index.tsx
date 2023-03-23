09c76dc60d37d353df979df3143e739a65dd873f
comstering <coms1101@gmail.com>
2023-03-23 14:07:12 +0900

    ✨ feat: Implement showing posts

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,9 +1,44 @@
+import { getSortedPostsData } from "@/lib/posts";
 import Head from "next/head";
+import Link from "next/link";
 
-const Blog = () => (
-    <Head>
-        <title>Comstering Blog</title>
-    </Head>
+export const getStaticProps = () => {
+    const allPostsData = getSortedPostsData();
+    return {
+        props: {
+            allPostsData
+        }
+    }
+};
+
+const Blog = ({ allPostsData }: {
+    allPostsData: {
+        id: string;
+        date: string;
+        title: string;
+    }[];
+}) => (
+    <div>
+        <Head>
+            <title>Comstering Blog</title>
+        </Head>
+        <section>
+            <h2>Blog</h2>
+            <ul>
+                {allPostsData.map(({ id, date, title }) => (
+                        <li key={id}>
+                            <Link href={`blog/post/${id}`}>
+                                {title}
+                                <br />
+                                {id}
+                                <br />
+                                {date}
+                            </Link>
+                        </li>
+                ))}
+            </ul>
+        </section>
+    </div>
 );
 
 export default Blog;
