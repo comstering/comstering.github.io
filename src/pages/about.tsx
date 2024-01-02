873b2f4b57266963844096aa9424bb043c2f383c
comstering <coms1101@gmail.com>
2024-01-02 20:02:35 +0900

    ✨ feat: Add about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,50 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+            <main>
+                <div>
+                    <div>
+                        안녕하세요!
+                        <br />
+                        
+                    </div>
+                    <div>
+                        <div>
+                            <div>Contact</div>
+                            <div>
+                                <div>email</div>
+                                <div>phone</div>
+                            </div>
+                        </div>
+                        <div>
+                            <div>Channel</div>
+                            <div>
+                                <div>blog</div>
+                                <div>github</div>
+                                <div>linkedin</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <div>Language</div>
+                            <div>
+                                <div>kotlin</div>
+                                <div>java</div>
+                            </div>
+                            <div>Framework</div>
+                            <div>
+                                <div>Spring boot</div>
+                                <div>Docker</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+};
+
+export default About;
