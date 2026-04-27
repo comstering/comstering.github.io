b497ca321a608f19e9c1dfd9cf94ad553bdc4866
comstering <comstering@gmail.com>
2026-04-28 01:01:11 +0900

    refactor: Change robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: "/",
+        allow: ["/", "/_next/static/"],
         disallow: ["/api/", "/_next/"],
       },
     ],
